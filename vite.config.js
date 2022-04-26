72df761624c408709cfb9c7f97e33918ee173152
Antonio Bogallo <antonio.bogallo@gmail.com>
2022-04-26 14:02:42 +0200

    Creacion del nav, header y la seccion de categorias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-const path = require("path");
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" ? "/" + path.basename(process.cwd()) + "/" : "/";
+const base = "/";
 
 module.exports = {
   root: "src",
