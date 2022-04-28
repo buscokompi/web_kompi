095c04feb2076d7804e9a410fd5ce1cf034d480d
Jose Fernando Alvarez <joseferalvarezromero@gmail.com>
2022-04-28 16:47:16 +0200

    funcionalidad login y signin anadida

---
+++ src/js/signin.js
@@ -0,0 +1,30 @@
+import { signInFirebase, logInGoogle, logInFacebook } from "./fbauth.js";
+
+const signInButton = document.querySelector(".btn-login-email");
+const logGoogle = document.querySelector(".btn-login-google");
+const logFacebook = document.querySelector(".btn-login-facebook");
+
+// Evento on click que coge el nombre de usuario y contraseña y lo registra en la base de datos de firebase
+signInButton.addEventListener("click", () => {
+  console.log("sign up en proceso");
+
+  const email = document.querySelector(".input-email").value;
+  const password = document.querySelector(".input-password").value;
+  const passwordCheck = document.querySelector(".input-password-check").value;
+
+  if (password !== passwordCheck) {
+    alert("Comprueba que la contraseña sea correcta");
+  } else {
+    signInFirebase(email, password);
+  }
+});
+
+// Login con google
+logGoogle.addEventListener("click", () => {
+  logInGoogle();
+});
+
+// Login con facebook
+logFacebook.addEventListener("click", () => {
+  logInFacebook();
+});
