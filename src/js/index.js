8bfc07367d1d793acfbeba55f9b23c11ddedb3d9
“Alexis <alexisplettener@gmail.com>
2022-05-04 21:16:05 +0100

    topbar

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,26 @@ question.forEach(question => {
     }
   });
 });
+
+// Header transparent to white when scroll
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".topbar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Burger menu for mobile
+
+const click = document.querySelectorAll(".menuClick");
+click.forEach(element => {
+  element.addEventListener("click", toggleMenu);
+});
+
+function toggleMenu() {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navigation = document.querySelector(".mobile-nav");
+  const logo = document.querySelector(".logo");
+
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+  logo.classList.toggle("active");
+}
