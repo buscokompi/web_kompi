6cfac5be5e805d0119b484fdfe615de49025dd40
Atteneri González <attenerigonmen@gmail.com>
2022-05-06 22:34:14 +0100

    problema con js ./js/form-user.js Atteneri

---
+++ src/js/form-user.js
@@ -0,0 +1,18 @@
+const answersForm = [["no tengo preferencia", "un perro", "un gato", "un ave", "un roedor", "un reptil"],
+  ["no tengo preferencia", "una hembra", "un macho"],
+  ["no tengo preferencia", "pequeña", "mediana", "grande", "gigante"],
+  ["no tengo preferencia", "muy activo", "activo", "poco activo"],
+  ["un piso", "una casa", "una casa con jardín"],
+  ["no tengo preferencia", "adiestrada", "sus vacunas al día", "microchip", "todas las anteriores"],
+  ["dispuesto/a", "no dispuesto/a"],
+  ["Es la primera vez que soy", "Actualmente soy", "Anteriormente fui"],
+  ["ninguna mascota", "un perro", "un gato", "un ave", "un roedor", "otro animal doméstico", "varios de los mencionados anteriormente"],
+  ["solo yo", "mi pareja", "mi pareja y mis hijos"],
+  ["no tengo hijos", "menores de 10", "mayores de 10", "menores y mayores"],
+  ["no hay restricciones", "hay restricciones"],
+  ["de 2 a 5 horas", "de 5 a 8 horas", "de 8 a 12 horas"],
+  ["tengo en mi entorno a alguien que cuide del animal", "no tengo a nadie que cuide del animal"]];
+
+// const questions = document.getElementsByClassName(".question");
+
+console.log("hola");
