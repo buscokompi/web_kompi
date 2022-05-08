05a07f01100bb005582fcb4ed4f998a12f7e87e9
Jose Fernando Alvarez <joseferalvarezromero@gmail.com>
2022-05-08 20:22:47 +0200

    funcionalidad form users

--- src/js/form-user.js
+++ src/js/form-user.js
@@ -1,5 +1,6 @@
 const answersForm = [["no tengo preferencia", "un perro", "un gato", "un ave", "un roedor", "un reptil"],
   ["no tengo preferencia", "una hembra", "un macho"],
+  ["no tengo preferencia", "cachorro", "joven", "adulta", "anciana"],
   ["no tengo preferencia", "pequeña", "mediana", "grande", "gigante"],
   ["no tengo preferencia", "muy activo", "activo", "poco activo"],
   ["un piso", "una casa", "una casa con jardín"],
@@ -13,13 +14,14 @@ const answersForm = [["no tengo preferencia", "un perro", "un gato", "un ave", "
   ["de 2 a 5 horas", "de 5 a 8 horas", "de 8 a 12 horas"],
   ["tengo en mi entorno a alguien que cuide del animal", "no tengo a nadie que cuide del animal"]];
 
-const questions = document.getElementsByClassName(".question");
+const questions = document.getElementsByClassName("question");
 
-console.log(questions);
-questions.forEach((e, index) => {
+for (let i = 0; i < questions.length; i++) {
+  console.log(typeof answersForm[i]);
   let html = "";
-  for (let i = 0; i < answersForm[index].length; i++) {
-    html += `<option value="${answersForm[index][i]}">${answersForm[index][i]}</option>`;
+  for (let e = 0; e < answersForm[i].length; e++) {
+    console.log(answersForm[i][e]);
+    html += `<option value="${answersForm[i][e]}">${answersForm[i][e]}</option>`;
   }
-  e.innerHTML = html;
-});
+  questions[i].innerHTML = html;
+};
