8dffe79d143fc530318d55ec565cbd9b335c7a04
Jose Fernando Alvarez <joseferalvarezromero@gmail.com>
2022-05-09 15:50:06 +0200

    options html

--- src/js/newuser.js
+++ src/js/newuser.js
@@ -40,7 +40,8 @@ auth.onAuthStateChanged(function(user) {
 
 const btnContinue = document.querySelector(".btn-continue");
 
-btnContinue.addEventListener("click", () => {
+btnContinue.addEventListener("click", (e) => {
+  e.preventDefault();
   const nameUser = document.querySelector(".input-name").value;
   const surnameUser = document.querySelector(".input-surname").value;
   const dniUser = document.querySelector(".input-dni").value;
@@ -50,13 +51,25 @@ btnContinue.addEventListener("click", () => {
   const directionUser = document.querySelector(".input-direction").value;
   const phoneUser = document.querySelector(".input-phone").value;
 
+  console.log(nameUser);
+  console.log(surnameUser);
+  console.log(dniUser);
+  console.log(birthdateUser);
+  console.log(provinceUser);
+  console.log(locationUser);
+  console.log(directionUser);
+  console.log(phoneUser);
+
   addUser(nameUser, surnameUser, dniUser, birthdateUser, provinceUser, locationUser, directionUser, phoneUser);
+  setTimeout(function() {
+    console.log("hola");
+    window.location.href = "options-newuser.html";
+  }, 4000);
 });
 
 export async function addUser(nameUser, surnameUser, dniUser, birthdateUser, provinceUser, locationUser, directionUser, phoneUser) {
   // await addDoc(collection(fs, "usuarios/" + logued), { name: nameUser, surname: surnameUser, nif: dniUser, birthdate: birthdateUser, province: provinceUser, location: locationUser, direction: directionUser, phone: phoneUser });
   await setDoc(doc(fs, "usuarios", userEmail), { name: nameUser, surname: surnameUser, nif: dniUser, birthdate: birthdateUser, province: provinceUser, location: locationUser, direction: directionUser, phone: phoneUser });
-  window.location.href = "options-newuser.html";
 }
 
 function actualDate() {
