3445385e1a81ee912fb4faaad811a299346d27e7
Antonio Bogallo <antonio.bogallo@gmail.com>
2022-05-12 16:32:43 +0200

    functionality done

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -1,6 +1,56 @@
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { initFirebase } from "./fbinit.js";
+const firebaseapp = initFirebase();
+
+const fs = getFirestore();
+const storage = getStorage(firebaseapp);
+
 const arraySections = document.querySelectorAll(".sections p");
 const arrayLines = document.querySelectorAll(".sections hr");
 const profileContent = document.querySelector(".profile-content");
+let archivo = null;
+
+async function addAnimal(info) {
+  subirFoto();
+  const imagen1 = "gs://web-kompi.appspot.com/animals/" + archivo.name;
+  await addDoc(collection(fs, "animals"), {
+    Nombre: info[0],
+    Especie: info[1],
+    Edad: info[2],
+    Raza: info[3],
+    Sexo: info[4],
+    Tamano: info[5],
+    Peso: info[6],
+    Color: info[7],
+    Pelo: info[8],
+    Microchip: info[9],
+    Vacunacion: info[10],
+    Esterilizacion: info[11],
+    Certificado_ppp: info[12],
+    Descripcion: info[14],
+    Imagen1: imagen1
+  });
+}
+
+function pickInfo() {
+  const inputArray = document.querySelectorAll(".formulario input");
+  const textArea = document.querySelector(".formulario textarea");
+  const info = [];
+  inputArray.forEach(element => {
+    info.push(element.value);
+  });
+  info.push(textArea.value);
+  console.log(info);
+
+  addAnimal(info);
+}
+
+function subirFoto() {
+  const refImg = ref(storage, "animals/" + archivo.name);
+
+  uploadBytes(refImg, archivo);
+}
 
 function chargeForm() {
   profileContent.innerHTML = /* html */`
@@ -59,14 +109,41 @@ function chargeForm() {
         </div>
         <div class="description">
           <h3>Decripción</h3>
-          <input type="text">
+          <textarea name="" placeholder="Escribe aquí"></textarea>
         </div>
         <div class="images">
           <h3>Fotografías</h3>
-          <input type="text">
+          <div class="uploadImages">
+          <input class="fileinput" type="file" value="Subir Archivo" accept="image/*">
+          </div>
+          
         </div>
     </form>
+
+    <div class="save">
+      <button class="submit">Guardar</button>
+    </div>
+    
     `;
+
+  const fileinput = document.querySelector(".fileinput");
+  fileinput.addEventListener("change", (e) => {
+    e.preventDefault();
+    archivo = e.target.files[0];
+  });
+
+  // const uploadImg = document.querySelector(".uploadImg");
+  // uploadImg.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   subirFoto();
+  // });
+
+  const btn = document.querySelector(".submit");
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    pickInfo();
+  }
+  );
 }
 
 function aboutMe() {
