548edca4da685520bc32d78a32dc7055d604984d
Juani <jrm3301@gmail.com>
2022-05-13 00:52:13 +0100

    ultimas modificaciones para ver las contraseñas de los loggins

--- src/js/signin.js
+++ src/js/signin.js
@@ -62,7 +62,7 @@ function signIn() {
     signInFirebase(email, password);
   }
 };
-// See password
+// See first password
 const password = document.getElementById("password");
 const togglePassword = document.getElementById("toggle-password");
 togglePassword.addEventListener("click", toggleClicked);
@@ -73,5 +73,19 @@ function toggleClicked() {
     password.type = "text";
   } else {
     password.type = "password";
+  }
+}
+
+// See second password
+const password2 = document.getElementById("password2");
+const togglePassword2 = document.getElementById("toggle-password2");
+togglePassword2.addEventListener("click", toggleClicked2);
+
+function toggleClicked2() {
+  password2.classList.toggle("visible");
+  if (this.checked) {
+    password2.type = "text";
+  } else {
+    password2.type = "password";
   }
 }
