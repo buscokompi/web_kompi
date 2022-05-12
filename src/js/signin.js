9d858c8c3b4087975c18a917e11411d1735fd8fe
Juani <jrm3301@gmail.com>
2022-05-13 00:18:28 +0100

    algunos media queries de los 2 login y mostrar contraseña en los dos loggin

--- src/js/signin.js
+++ src/js/signin.js
@@ -62,3 +62,16 @@ function signIn() {
     signInFirebase(email, password);
   }
 };
+// See password
+const password = document.getElementById("password");
+const togglePassword = document.getElementById("toggle-password");
+togglePassword.addEventListener("click", toggleClicked);
+
+function toggleClicked() {
+  password.classList.toggle("visible");
+  if (this.checked) {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
