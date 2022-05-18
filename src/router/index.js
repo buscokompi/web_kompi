e18113204a4e73ed20aec91778c274687f534551
Jose Fernando Alvarez <joseferalvarezromero@gmail.com>
2022-05-18 23:02:35 +0200

    errores index e icono

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: () => import("")
+      component: () => import("../views/ForgotPassword.vue")
     },
   ]
 });
