505265a0830c1cecee61a848c382488fd51ab8b6
Juani <jrm3301@gmail.com>
2022-05-18 23:37:01 +0100

    login con media queries responsive (mobile first) falta añadir js

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,319 +1,312 @@
 <template>
-<div class="app">
-  <img class="img-dog-login" src="../assets/images/foto_perro.svg" alt="Fotografía perro Pug">
+  <div class="app">
+    <img class="img-dog-login" src="../assets/images/foto_perro.svg" alt="Fotografía perro Pug">
 
-  <div class="container-login">
+    <div class="container-login">
 
-    <a href="../index.html">
-      <img class="logo" src="../assets/icons/version_negro_logo.svg" alt="Logotipo Kompi">
-    </a>
+      <a href="../index.html">
+        <img class="logo" src="../assets/icons/version_negro_logo.svg" alt="Logotipo Kompi">
+      </a>
 
-    <div class="card-login">
-     <form id="login">
+      <div class="card-login">
 
-</form>
-<div>
-      <p class="email">E-mail</p>
-      <input class="input-email" placeholder="adoptaunkompi@gmail.com" >
+        <p class="email">E-mail</p>
+        <input class="input-email" placeholder="adoptaunkompi@gmail.com">
         <p class="pass">Contraseña</p>
-        <input type="password" id="password" placeholder="BuscoKompi6."/>
+        <input type="password" id="password" placeholder="BuscoKompi6." />
         <input type="checkbox" id="toggle-password" />
         <label for="toggle-password"></label>
+
+       <a class="forgot-pass" href="./forgot-password.html">Has olvidado tu contraseña?</a>
+        <div class="button btn-login-email"><span>Continuar</span></div>
+        <div class="button btn-login-google"><img class="google" src="../assets/icons/google_icono.svg"
+           alt="Icono Google"><span>Inicia sesión con Google</span></div>
+
       </div>
-
-
-      <p class="forgot-pass"><a  href="./forgot-password.html">Has olvidado tu contraseña?</a></p>
-      <div class="button btn-login-email"><span>Continuar</span></div>
-      <div class="button btn-login-google"><img class="google" src="../assets/icons/google_icono.svg"
-          alt="Icono Google"><span>Inicia sesión con Google</span></div>
-
+      <p class="register">¿No tienes una cuenta? <a class="link-signin" href="../signin.html">Regístrate</a></p>
     </div>
-    <p class="register">¿No tienes una cuenta? <a class="link-signin" href="../signin.html">Regístrate</a></p>
   </div>
-</div>
 
 </template>
 
 <script>
-    export default {
+  export default {
 
-    }
-
+  }
 </script>
 
 <style scoped>
-:root {
-  --orange: #f9b225;
-  --yellow: #ffe194;
-  --black: #3f3d3b;
-  --white: white;
-  --grey: #c4c4c4;
-  --lightgrey: #f5f5f5;
-  --green: #015865;
-  --title-font: "Dosis";
-  --text-font: "Open Sans";
-}
-
-.app {
-  display: flex;
-  flex-direction: row;
-  margin: 0;
-  height: 100vh;
-  background-color: #f5f5f5;
-}
-
-.img-dog-login {
-  width: 45%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.container-login {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 50%;
-  margin-left: 3.6rem;
-}
-
-.logo {
-  width: 8rem;
-  margin: 2rem 0;
-  cursor: pointer;
-}
-
-.card-login {
-  display: flex;
-  width: 28rem;
-  height: 40rem;
-  flex-direction: column;
-  border-radius: 1.9rem;
-  padding: 3.5rem 5rem;
-  background-color: white;
-  margin-bottom: 2rem;
-}
-
-.input-email {
-  width: 27rem;
-}
-
-#password {
-  width: 27rem;
-}
-
-@media screen and (max-width: 970px) {
-  .logo {
-    margin-left: 6rem;
+  :root {
+    --orange: #f9b225;
+    --yellow: #ffe194;
+    --black: #3f3d3b;
+    --white: white;
+    --grey: #c4c4c4;
+    --lightgrey: #f5f5f5;
+    --green: #015865;
+    --title-font: "Dosis";
+    --text-font: "Open Sans";
   }
 
-  .card-login {
-    height: 65%;
+  .app {
     display: flex;
-    margin-left: 8rem;
-    width: 17rem;
-  }
-
-  .input-email {
-    width: 16rem;
-  }
-
-  .btn-signin-facebook {
-    margin-top: 1rem;
-  }
-
-  #password {
-    width: 16rem;
-  }
-
-  .btn-signin-email {
-    padding: 0.8rem;
-  }
-
-  .btn-login-facebook {
-    margin-top: 1rem;
-  }
-
-  .btn-login-google {
-    margin-top: 2rem;
-  }
-
-  .register {
-    margin-left: 10rem;
-    width: 20rem;
+    flex-direction: row;
+    margin: 0;
+    height: 100vh;
+    background-color: #f5f5f5;
+    justify-content: center;
   }
 
   .img-dog-login {
     display: none;
   }
-}
 
-.card-login p {
-  color: var(--black);
-  font-family: var(--text-font);
-}
+  .container-login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 70%;
+  }
+
+  .logo {
+    width: 8rem;
+    margin: 2rem 0;
+    cursor: pointer;
+  }
+
+  .card-login {
+    display: flex;
+    align-items: center;
+    width: 100vw;
+    height: 40rem;
+    flex-direction: column;
+    border-radius: 1.9rem;
+    background-color: white;
+  }
+
+    .email{
+      margin-top: 2.5rem;
+    }
+  .card-login p {
+    width: 75%;
+    min-width: 250px ;
+    color: var(--black);
+    font-family: var(--text-font);
+  }
+
+  input {
+    width: 75%;
+    min-width: 250px ;
+    height: 2.5rem;
+    border-radius: 0.8rem;
+    border: var(--grey) 0.1rem solid;
+    font-family: var(--text-font);
+    font-size: 1rem;
+  }
+
+  input:focus {
+    color: var(--black);
+    outline-color: var(--black);
+  }
+
+  .button {
+    width: 75%;
+    min-width: 250px ;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    height: 3rem;
+    border-radius: 0.8rem;
+    border: 0;
+    font-size: 1rem;
+    font-family: var(--text-font);
+    font-weight: 600;
+    transition: background-color 0.7s ease;
+  }
+
+  .btn-login-email {
+    background-color: var(--orange);
+    margin-top: 2rem;
+    margin-bottom: 3rem;
+    justify-content: center;
+  }
+
+  .button span {
+    color: var(--black);
+    pointer-events: none;
+  }
+
+  .btn-login-email:hover {
+    background-color: #cc9320;
+  }
+
+  .btn-login-email:hover span {
+    color: var(--white-color);
+  }
+
+  /* BOTONES LOGIN */
+
+  .btn-login-google {
+    display: flex;
+    gap: 0.5rem;
+    justify-content: center;
+    background-color: var(--white-color);
+    border: var(--grey) 0.06rem solid;
+    color: var(--black);
+    margin-bottom: 1rem;
+  }
+
+  .btn-login-google:hover {
+    background-color: #bebebe;
+  }
+
+  .btn-login-google span {
+    color: var(--black);
+  }
+
+  /* BOTONES SIGN IN */
+
+  .btn-signin-email:hover {
+    background-color: #cc9320;
+  }
+
+  .btn-signin-google:hover {
+    background-color: #bebebe;
+  }
+
+  .btn-signin-google span {
+    color: var(--black);
+  }
+
+  .link-signin {
+    color: var(--green);
+    font-family: var(--text-font);
+    font-weight: 600;
+  }
+
+  .google {
+    width: 1.8rem;
+    height: 1.8rem;
+  }
+
+  p {
+    font-family: var(--text-font);
+    color: var(--black);
+    font-size: 0.9rem;
+  }
+
+  .forgot-pass {
+    font-family: var(--text-font);
+    font-size: 0.8rem;
+    width: 85%;
+    text-decoration: underline;
+    color: var(--black);
+  }
 
 
-input {
-  padding: 0.5rem;
-  border-radius: 0.8rem;
-  border: var(--grey) 0.1rem solid;
-  font-family: var(--text-font);
-  font-size: 1rem;
-}
 
-input:focus {
-  color: var(--black);
-  outline-color: var(--black);
-}
+  #message {
+    font-family: var(--text-font);
+    font-size: 0.9rem;
+  }
 
-.button {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  height: 3rem;
-  border-radius: 0.8rem;
-  border: 0;
-  font-size: 1rem;
-  font-family: var(--text-font);
-  font-weight: 600;
-  transition: background-color 0.7s ease;
-}
+  #comment {
+    font-family: var(--text-font);
+    font-size: 0.8rem;
+  }
 
-.button img {
-  object-fit: contain;
-  margin-right: 1.5rem;
-}
+  /* see password */
 
-.btn-login-email {
-  background-color: var(--orange);
-  margin-top: 2rem;
-  margin-bottom: 3rem;
-  justify-content: center;
-}
+  #password {
+    margin-top: 1rem;
+    background-image: url("https://img.icons8.com/material-sharp/20/000000/visible.png");
+    background-position: 97% center;
+    background-repeat: no-repeat;
+  }
 
-.button span {
-  color: var(--black);
-  pointer-events: none;
-}
+  #password.visible {
+    background-image: url("https://img.icons8.com/material-outlined/20/000000/invisible.png");
+  }
 
-.btn-login-email:hover {
-  background-color: #cc9320;
-}
+  #toggle-password {
+    display: none;
+  }
 
-.btn-login-email:hover span {
-  color: var(--white-color);
-}
+  .pass {
+    margin-top: 2rem;
+    margin-bottom: -1px;
+  }
 
-/* BOTONES LOGIN */
+  #toggle-password+label {
+    text-indent: -9999px;
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    margin-left: -32px;
+    cursor: pointer;
+  }
 
-.btn-login-google {
-  background-color: var(--white-color);
-  border: var(--grey) 0.06rem solid;
-  color: var(--black);
-  margin-bottom: 1rem;
-  margin-top: rem;
-}
+  .register {
+    text-align: center;
+  }
 
-.btn-login-google:hover {
-  background-color: #bebebe;
-}
 
-.btn-login-google span {
-  color: var(--black);
-}
-/* BOTONES SIGN IN */
 
-.btn-signin-email {
-  background-color: var(--orange);
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-  justify-content: center;
-}
+  /* --------------------------------------------------------------- MEDIA QUERIES ----------------------------------------------------------- */
+  @media screen and (min-width: 767px) {
+    .card-login {
+      width: 100%
+    }
 
-.btn-signin-email:hover {
-  background-color: #cc9320;
-}
+    input,
+     .button,
+     .card-login p,
+     .forgot-pass {
+      width: 75%;
+    }
 
-.btn-signin-google {
-  background-color: var(--white);
-  border: var(--grey) 0.06rem solid;
-  color: var(--black);
-  margin-bottom: 1rem;
-}
+  }
+  @media screen and (min-width: 1170px) {
 
-.btn-signin-google:hover {
-  background-color: #bebebe;
+    .img-dog-login{
+      object-fit: cover;
+      width: 45%;
+      height: 100%;
+      display:block
+    }
+    input,
+     .button,
+     .card-login p,
+     .forgot-pass {
+      width: 80%;
+    }
+  .card-login{
+    width: 28rem;
+    height: 40rem;
+    padding: 3.5rem 5rem;
+  }
+.btn-login-email{
+  margin-top: 5rem;
 }
+  }
 
-.btn-signin-google span {
-  color: var(--black);
-}
-.link-signin {
-  color: var(--green);
-  font-family: var(--text-font);
-  font-weight: 600;
-}
+  @media screen and (min-width: 1300px) {
 
-.google {
-  width: 1.8rem;
-  height: 1.8rem;
-  margin-left: 20%;
-}
+  .img-dog-login{
+      display:block
+    }
 
-p {
-  font-family: var(--text-font);
-  color: var(--black);
-  font-size: 0.9rem;
-}
+  }
 
-.forgot-pass {
-  font-family: var(--text-font);
-  font-size: 0.8rem;
-}
+  input,
+     .button,
+     .card-login p,
+     .forgot-pass {
+      width: 90%;
+    }
 
-.forgot-pass a {
-  text-decoration: underline;
-  color: var(--black);
-}
+    .register{
+      margin-top: 2rem;
+    }
 
-#message {
-  font-family: var(--text-font);
-  font-size: 0.9rem;
-}
-
-#comment {
-  font-family: var(--text-font);
-  font-size: 0.8rem;
-}
-
-/* see password */
-
-#password {
-  margin-top: 1rem;
-  background-image: url("https://img.icons8.com/material-sharp/20/000000/visible.png");
-  background-position: 97% center;
-  background-repeat: no-repeat;
-}
-
-#password.visible {
-  background-image: url("https://img.icons8.com/material-outlined/20/000000/invisible.png");
-}
-
-#toggle-password {
-  display: none;
-}
-.pass {
-  margin-top: 2rem;
-  margin-bottom: -1px;
-}
-
-#toggle-password + label {
-  text-indent: -9999px;
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  margin-left: -32px;
-  cursor: pointer;
-}
 </style>
-
