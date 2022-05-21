670f492a603b8c585493935dacd66ba58ff8be48
“Alexis <alexisplettener@gmail.com>
2022-05-21 14:25:21 +0100

    pnpm install

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,23 +1,23 @@
 <template>
 
-    <RouterLink class="button" :to="buttonUrl">{{buttonText}}</RouterLink>
+    <RouterLink class="button" :to="buttonUrl">{{ buttonText }}</RouterLink>
 
 </template>
 
 <script>
 export default {
     props: {
-       url: {
-           type: String,
-           required: true
-       },
-       text:{
-           type: String,
-           required: true
-       }
+        url: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
     },
-    data(){
-        return{
+    data() {
+        return {
             buttonUrl: this.url,
             buttonText: this.text
         }
@@ -28,20 +28,19 @@ export default {
 
 <style scoped>
 .button {
-  background: var(--orange);
-  color: var(--black);
-  padding: 0.8rem 2rem;
-  display: inline-block;
-  text-decoration: none;
-  border-radius: 3rem;
-  font-family: var(--text-font);
-  font-size: 1em;
-  font-weight: 600;
-  transition: 0.5s ease-out;
+    background: var(--orange);
+    color: var(--black);
+    padding: 0.8rem 2rem;
+    display: inline-block;
+    text-decoration: none;
+    border-radius: 3rem;
+    font-family: var(--text-font);
+    font-size: 1em;
+    font-weight: 600;
+    transition: 0.5s ease-out;
 }
 
 .button:hover {
-  background: #cc9320;
+    background: #cc9320;
 }
-
 </style>
