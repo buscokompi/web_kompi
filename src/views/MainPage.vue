f0b90c811142e26e532fde3013bc33e92c7ec3f1
“Alexis <alexisplettener@gmail.com>
2022-05-21 20:49:00 +0100

    accordion list

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -14,16 +14,15 @@ import MainMain from '../components/MainMain.vue'
 import MainCategory from '../components/MainCategory.vue'
 import MainAdoption from '../components/MainAdoption.vue'
 import MainCounter from '../components/MainCounter.vue'
-import MainList from '../components/MainList.vue'
 import MainGuide from '../components/MainGuide.vue'
+import MainList from '../components/MainList.vue'
 import TheFooter from '../components/TheFooter.vue'
 import TheHeader from '../components/TheHeader.vue'
-    export default {
-    components: { TheHeader ,MainMain, MainCategory, MainAdoption, MainCounter, MainGuide, MainList, TheFooter}
+export default {
+    components: { TheHeader, MainMain, MainCategory, MainAdoption, MainCounter, MainGuide, MainList, TheFooter }
 }
 
 </script>
 
 <style scoped>
-
 </style>
