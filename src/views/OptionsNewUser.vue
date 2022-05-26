ba7112fe630cd3e40290fc1391f106724e17dc6d
Juani <jrm3301@gmail.com>
2022-05-26 13:24:55 +0100

    arreglar adoption-new-user darle estilos a las dos cards

--- src/views/OptionsNewUser.vue
+++ src/views/OptionsNewUser.vue
@@ -9,11 +9,11 @@
       <p class="question1">¿Qué es lo que quieres hacer?</p>
     </div>
     <div class="card-options">
-      <RouterLink to="/FormUser">
+      <RouterLink class="adopt" to="/FormUser">
         <img src="../assets/icons/icono_logo_negro.svg" alt="Isologo">
         <p>ADOPTAR</p>
       </RouterLink>
-      <RouterLink to="/NewAnimalForm">
+      <RouterLink class="give-in-adoption" to="/NewAnimalForm">
         <img src="../assets/icons/icono_logo_blanco.svg" alt="Isologo">
         <p>DAR EN ADOPCIÓN</p>
       </RouterLink>
